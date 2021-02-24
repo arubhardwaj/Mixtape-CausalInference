9abe06a1cecc2bd9d369f3329dac4cbf3ea06bea
Parker Knight <plb.knight@gmail.com>
2021-02-24 15:41:59 -0500

    Add model to collider_discrimination

--- Code/Chapter3/collider_discrimination.jl
+++ Code/Chapter3/collider_discrimination.jl
@@ -4,3 +4,15 @@ n = 10000;
 
 female = rand(Normal(), n) .> 0
 ability = rand(Normal(), n)
+discrimination = female
+
+occupation = 1 .+ 2 .* ability + 0 .* female - 2 .* discrimination + rand(Normal(), n)
+wage = 1 .- discrimination + occupation + 2 .* ability + rand(Normal(), n)
+
+dat = DataFrame(female = female,ability = ability,discrimination = discrimination, occupation = occupation, wage = wage)
+
+
+
+lm1 = lm(@formula(wage ~ female), dat)
+lm2 = lm(@formula(wage ~ female + occupation), dat)
+lm3 = lm(@formula(wage ~ female + occupation + ability), dat)
